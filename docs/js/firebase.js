559cdfa107182ccf7c39a5d4cd9e582c35787385
ChalkStreamLearn <aungkhurtai1781@gmail.com>
2026-08-03 21:33:16 +0630

    Add files via upload

--- docs/js/firebase.js
+++ docs/js/firebase.js
@@ -14,14 +14,6 @@ import {
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAKOajhx7vxLXUMFE3Vu6w9XZS74tLoq1s",
   authDomain: "chalkstream-sports.firebaseapp.com",
@@ -31,10 +23,6 @@ const firebaseConfig = {
   appId: "1:700889174885:web:dcc39ab8b069b80758478c",
   measurementId: "G-2XTG0N1K2P"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const app = initializeApp(firebaseConfig);
 
